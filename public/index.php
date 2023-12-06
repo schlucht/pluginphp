05bd0f45aca2216bbc756cac36c1a3d488ea64e2
Schlucht <web@jagolo.ch>
2023-12-06 05:55:12 +0100

    start Plugin Projekt

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <title>My Document</title>
+</head>
+<body>
+    <h1>Hallo Document</h1>
+    <h2><?= __DIR__?></h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad asperiores, voluptatibus a assumenda aspernatur doloribus provident ratione nobis, saepe velit corporis possimus, eius rem voluptatem accusamus pariatur vel sunt dolorum.</p>
+</body>
+</html>
